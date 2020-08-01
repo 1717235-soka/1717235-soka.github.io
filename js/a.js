15d6f19bdace624fcfdd428479392f90e337f8e3
1717235-soka <68987046+1717235-soka@users.noreply.github.com>
2020-08-01 22:36:28 +0900

    Update a.js

--- js/a.js
+++ js/a.js
@@ -1,6 +1,6 @@
      
             function getImage(){
-    var arr=['img/onigiri_ume.png' , 'img/onigiri_sake.png' , 'img/onigiri_seachicken.ping' , 'img/onigiri_tarako.png' , 'img/supamusubi.png', 'img/onigiri_yukari.png', 'img/onigiri_gomoku.png' , 'img/onigiri_ikura.png'  ]
+    var arr=['img/onigiri_ume.png' , 'img/onigiri_sake.png' , 'img/onigiri_seachicken.ping' , 'img/onigiri_tarako.png' , 'img/supamusubi.png', 'img/onigiri_yukari.png', 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_gomoku.png?raw=true' , 'img/onigiri_ikura.png'  ]
     
     var obj =document.getElementById("onigiri");
     var a =Math.floor(Math.random() * arr.length);
