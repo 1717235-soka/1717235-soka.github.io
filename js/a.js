6f421c14379372cd106d87cb10c71396759486e7
1717235-soka <68987046+1717235-soka@users.noreply.github.com>
2020-08-01 22:47:37 +0900

    Update a.js

--- js/a.js
+++ js/a.js
@@ -1,10 +1,8 @@
      
             function getImage(){
-    var arr=['img/onigiri_ume.png' , 'img/onigiri_sake.png' , 'img/onigiri_seachicken.ping' , 'img/onigiri_tarako.png' , 'img/supamusubi.png', 'img/onigiri_yukari.png', 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_gomoku.png?raw=true' , 'img/onigiri_ikura.png'  ]
+    var arr=['https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_ume.png?raw=true' , 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_sake.png?raw=true' , 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_seachicken.png?raw=true' , 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_tarako.png?raw=true' ,'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/supamusubi.png?raw=true', 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_yukari.png?raw=true', 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_gomoku.png?raw=true' , 'https://github.com/1717235-soka/1717235-soka.github.io/blob/master/img/onigiri_ikura.png?raw=true'  ]
     
     var obj =document.getElementById("onigiri");
     var a =Math.floor(Math.random() * arr.length);
     obj.src=arr[a];
-    
-  
 }   
